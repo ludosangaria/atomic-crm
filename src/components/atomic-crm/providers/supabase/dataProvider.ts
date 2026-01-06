0569cc6a07fc77cc80fb3a0950bc4f92bfc3b007
ludosangaria <ludovic_sangaria@mc3.fr>
2026-01-06 19:06:39 +0400

    Update dataProvider.ts

--- src/components/atomic-crm/providers/supabase/dataProvider.ts
+++ src/components/atomic-crm/providers/supabase/dataProvider.ts
@@ -27,13 +27,13 @@ import { supabase } from "./supabase";
 if (import.meta.env.VITE_SUPABASE_URL === undefined) {
   throw new Error("Please set the VITE_SUPABASE_URL environment variable");
 }
-if (import.meta.env.VITE_SUPABASE_ANON_KEY === undefined) {
-  throw new Error("Please set the VITE_SUPABASE_ANON_KEY environment variable");
+if (import.meta.env.VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY === undefined) {
+  throw new Error("Please set the VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY environment variable");
 }
 
 const baseDataProvider = supabaseDataProvider({
   instanceUrl: import.meta.env.VITE_SUPABASE_URL,
-  apiKey: import.meta.env.VITE_SUPABASE_ANON_KEY,
+  apiKey: import.meta.env.VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY,
   supabaseClient: supabase,
   sortOrder: "asc,desc.nullslast" as any,
 });

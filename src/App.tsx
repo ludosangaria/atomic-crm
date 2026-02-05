038f04c85aa3e3f5e5a038d91536a7fb95fedb36
ludosangaria <ludovic_sangaria@mc3.fr>
2026-02-05 12:04:27 +0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -25,6 +25,29 @@ import { CRM } from "@/components/atomic-crm/root/CRM";
  *    />
  * );
  */
-const App = () => <CRM />;
+const App = () => (
+    <CRM
+        contactGender={[
+            { value: 'male', label: 'He' },
+            { value: 'female', label: 'She' },
+        ]}
+        companySectors={['Partenaire', 'Client Final', 'Fournisseur', 'Autres"]}
+        dealCategories={['HIT', 'Réseaux','Sécurité','Cross BU']}
+        dealPipelineStatuses={['won']}
+        dealStages={[
+            { value: 'opportunity', label: 'Leads' },
+            { value: 'proposal-sent', label: 'Proposition envoyée' },
+            { value: 'won', label: 'Gagné' },
+            { value: 'lost', label: 'Perdu' },
+          { value: 'delayed', label: 'Repoussé' },
+        ]}
+        noteStatuses={[
+            { value: 'cold', label: 'Cold', color: '#7dbde8' },
+            { value: 'warm', label: 'Warm', color: '#e8cb7d' },
+            { value: 'hot', label: 'Hot', color: '#e88b7d' },
+        ]}
+        taskTypes={['Appel', 'Email', 'RDV']}
+    />
+);
 
 export default App;
